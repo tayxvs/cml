9e53319ebe6e69d9b03f68213d7a5680c80ae528
tayxvs <tay29it@gmail.com>
2026-04-08 18:40:27 +0900

    v1

---
+++ train.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix
+from xgboost import XGBClassifier, plot_importance
+
+
+df = pd.read_csv("titanic.csv")
+df = df[['Survived', 'Pclass', 'Sex', 'Age', 'Fare']]
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+df['Age'] = df['Age'].fillna(df['Age'].median())
+
+
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+model = XGBClassifier()
+model.fit(X_train, y_train)
+
+
+pred = model.predict(X_test)
+acc = accuracy_score(y_test, pred)
+print("Accuracy:", acc)
+
+
+cm = confusion_matrix(y_test, pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+plt.title("Confusion Matrix")
+plt.savefig("plot1.png")
+
+
+plot_importance(model)
+plt.title("Feature importanace")
+plt.savefig("plot2.png")
